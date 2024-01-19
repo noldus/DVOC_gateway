52a3312261172acd5fa2746603876390f2f1f22b
Gert Buursink <gert.buursink@noldus.com>
2024-01-19 11:48:43 +0100

    changed timeout response

--- examples/rp2040/pico-rndis-dashboard/main.c
+++ examples/rp2040/pico-rndis-dashboard/main.c
@@ -1,11 +1,13 @@
 // Copyright (c) 2022 Cesanta Software Limited
 // All rights reserved
 
+#include "hardware/uart.h"
 #include "mongoose.h"
 #include "net.h"
 #include "pico/stdlib.h"
-#include "hardware/uart.h"
 #include "tusb.h"
+
+#define ERRORTIMEOUT "2;:"
 
 static struct mg_tcpip_if *s_ifp;
 
@@ -16,25 +18,33 @@ static void blink_cb(void *arg) {  // Blink periodically
   (void) arg;
 }
 
-static void cb(struct mg_connection *con, int ev, void *ev_data, void *fn_data) {
+static void cb(struct mg_connection *con, int ev, void *ev_data,
+               void *fn_data) {
   bool valid_response_received = false;
 
   if (ev == MG_EV_READ) {
     struct mg_iobuf *r = &con->recv;
-    //transmit over hardware uart
-    for(size_t i = 0; i < r->len; i++) {
+    // transmit over hardware uart
+    for (size_t i = 0; i < r->len; i++) {
       uart_putc(uart1, r->buf[i]);
     }
 
-    //make local variables
-    unsigned char buff[50] = {0};
+    // make local variables
+    unsigned char buff[100] = {0};
     size_t buffindex = 0;
 
-    if(uart_is_readable_within_us(uart1, 1000))  {
-      while(!(uart_getc(uart1) == '$')) {;;}
-    }
-    else {
-      mg_send(con, "err uartrx", 11);
+    // send error response if no response was received after 50ms from main
+    // controller
+    if (uart_is_readable_within_us(uart1, 100000)) {
+      while (!(uart_getc(uart1) == '$')) {
+        ;
+        ;
+      }
+    } else {
+      mg_send(con, ERRORTIMEOUT, 11);
+      for (int i = 0; i < 100; i++) {
+        buff[i] = 0;
+      }
       return;
     }
 
@@ -42,7 +52,7 @@ static void cb(struct mg_connection *con, int ev, void *ev_data, void *fn_data) 
     while (!valid_response_received && buffindex < sizeof(buff) - 1) {
       char c = uart_getc(uart1);
       if (c == ':') {
-        valid_response_received = true; // Set the flag when : is encountered
+        valid_response_received = true;  // Set the flag when : is encountered
         buff[buffindex++] = c;
       } else {
         buff[buffindex++] = c;
@@ -51,7 +61,7 @@ static void cb(struct mg_connection *con, int ev, void *ev_data, void *fn_data) 
 
     r->len = 0;
 
-    buff[buffindex] = '\0'; // End string
+    buff[buffindex] = '\0';  // End string
     MG_INFO(("received: %s", buff));
     mg_send(con, buff, buffindex);
   }
@@ -65,7 +75,8 @@ bool tud_network_recv_cb(const uint8_t *buf, uint16_t len) {
   return true;
 }
 
-void tud_network_init_cb(void) {}
+void tud_network_init_cb(void) {
+}
 
 uint16_t tud_network_xmit_cb(uint8_t *dst, void *ref, uint16_t arg) {
   // MG_INFO(("SEND %hu", arg));
@@ -90,7 +101,6 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_dta) {
   if (ev == MG_EV_HTTP_MSG) return mg_http_reply(c, 200, "", "ok\n");
 }
 
-
 int main(void) {
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
@@ -103,19 +113,20 @@ int main(void) {
 
   struct mg_tcpip_driver driver = {.tx = usb_tx, .up = usb_up};
   struct mg_tcpip_if mif = {.mac = {2, 0, 1, 2, 3, 0x77},
-                       .ip = mg_htonl(MG_U32(192, 168, 20, 206)),
-                       .mask = mg_htonl(MG_U32(255, 255, 255, 0)),
-                       .enable_dhcp_server = true,
-                       .driver = &driver,
-                       .recv_queue.size = 4096};
+                            .ip = mg_htonl(MG_U32(192, 168, 20, 206)),
+                            .mask = mg_htonl(MG_U32(255, 255, 255, 0)),
+                            .enable_dhcp_server = true,
+                            .driver = &driver,
+                            .recv_queue.size = 4096};
   s_ifp = &mif;
   mg_tcpip_init(&mgr, &mif);
   tusb_init();
 
   MG_INFO(("Initialising application..."));
-  web_init(&mgr);
+  // remove dashboard for now
+  // web_init(&mgr);
 
-  uart_init(uart1, 1000000);
+  uart_init(uart1, 100000);
   gpio_set_function(4, GPIO_FUNC_UART);
   gpio_set_function(5, GPIO_FUNC_UART);
   uart_set_hw_flow(uart1, false, false);
